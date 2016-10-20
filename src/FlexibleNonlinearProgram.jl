a3ccf1bd98953ded2032f6cb2326840319c15ce0
Twan Koolen <koolen.twan@gmail.com>
2016-10-20 04:57:12 -0400

    FlexibleNonlinearProgram.jl generated files.
        license:  MIT
        authors:  Twan Koolen
        years:    2016
        user:     
    
    Julia Version 0.5.0 [3c9d753]

---
+++ src/FlexibleNonlinearProgram.jl
@@ -0,0 +1,5 @@
+module FlexibleNonlinearProgram
+
+# package code goes here
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using FlexibleNonlinearProgram
+using Base.Test
+
+# write your own tests here
+@test 1 == 2
